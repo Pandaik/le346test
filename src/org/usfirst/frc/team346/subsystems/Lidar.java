b44b2cca28ebff93ba9115a78d9f31548f4b4754
emanfrazier <32434465+ethanfrazier@users.noreply.github.com>
2018-01-27 14:00:43 -0500

    actually made thing happen i think

--- src/org/usfirst/frc/team346/subsystems/Lidar.java
+++ src/org/usfirst/frc/team346/subsystems/Lidar.java
@@ -1,4 +1,4 @@
-package org.usfirst.frc.PUT_YOUR_TEAM_HERE.robot;
+
 
 import edu.wpi.first.wpilibj.SensorBase;
 import edu.wpi.first.wpilibj.I2C;
@@ -41,75 +41,3 @@ public void initDefaultCommand() {
 
 
 }
-
-Here is a simple robot code to demonstrate:
-
-package org.usfirst.frc.PUT_YOUR_TEAM_HERE.robot;
-
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-
-/**
-* The VM is configured to automatically run this class, and to call the
-* functions corresponding to each mode, as described in the IterativeRobot
-* documentation. If you change the name of this class or the package after
-* creating this project, you must also update the manifest file in the resource
-* directory.
-*/
-public class Robot extends IterativeRobot{
-
-LidarSubsystem lidar1;
-
-/**
-* This function is run when the robot is first started up and should be
-* used for any initialization code.
-*/
-public void robotInit() {
-
-lidar1=new LidarSubsystem();
-
-}
-
-/**
-* This function is run once each time the robot enters autonomous mode
-*/
-public void autonomousInit() {
-}
-
-/**
-* This function is called periodically during autonomous
-*/
-public void autonomousPeriodic() {
-
-int distance;
-
-distance = lidar1.getDistance();
-System.out.println(distance); // this prints to the console window.
-
-}
-
-/**
-* This function is called once each time the robot enters tele-operated mode
-*/
-public void teleopInit(){
-}
-
-/**
-* This function is called periodically during operator control
-*/
-public void teleopPeriodic() {
-
-}
-
-/**
-* This function is called periodically during test mode
-*/
-public void testPeriodic() {
-LiveWindow.run();
-}
-
-}
-

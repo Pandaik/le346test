abd71b95ac3b2e7d5590763eb3c85c859b9956b2
Gavin <pandaikthepanda@gmail.com>
2018-01-28 11:18:01 -0500

    saved Changes

--- src/org/usfirst/frc/team346/control/SingleStickControl.java
+++ src/org/usfirst/frc/team346/control/SingleStickControl.java
@@ -1,5 +1,40 @@
 package org.usfirst.frc.team346.control;
 
+import org.usfirst.frc.team346.robot.Drive;
+
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.XboxController;
+
 public class SingleStickControl {
+	int sideRight;
+	int sideLeft;
+	int dir;
+	Drive driveTrain;
+	XboxController xbxcontrol;
+	public SingleStickControl(int controller) {
+		xbxcontrol = new XboxController(controller);
+	}
+//	public void ControllerDropout() {
+//		if(xbxcontrol.getY(Hand.kLeft) <5 && xbxcontrol.getY(Hand.kLeft))
+//	}
+	public double ControllerOutputR() {
+		
+		//forward = -1
+		//backwards = 1
+		
+		if(xbxcontrol.getY(Hand.kLeft) > 0) {
+			dir = 1;
+		}
+		if(xbxcontrol.getY(Hand.kLeft) < 0) {
+			dir = -1;
+		}
+		else {
+			dir = 0;
+		}
+		
+		if(dir!=0) {
+			
+		}
+	}
 
 }
